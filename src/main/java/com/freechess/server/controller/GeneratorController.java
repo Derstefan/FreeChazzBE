cf8ec4ccc75bda3e2b6669bb09a86a63cf4bc3b8
Stefan Wetzig <stefanwetzig@yahoo.de>
2022-07-11 23:43:05 +0200

    added crossorigin again

--- src/main/java/com/freechess/server/controller/GeneratorController.java
+++ src/main/java/com/freechess/server/controller/GeneratorController.java
@@ -8,7 +8,7 @@ import com.freechess.generators.piece.impl.PieceTypeGeneratorPool;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin("*")
+@CrossOrigin(origins = "https://free-chazz-be.herokuapp.com")
 @RestController
 @RequestMapping("api/")
 public class GeneratorController {
--- src/main/java/com/freechess/server/controller/ServerController.java
+++ src/main/java/com/freechess/server/controller/ServerController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin("*")
+@CrossOrigin(origins = "https://free-chazz-be.herokuapp.com")
 @RestController
 @RequestMapping("api/")
 public class ServerController {
