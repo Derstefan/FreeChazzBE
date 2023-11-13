604feb3a523d14c28ac11ca9eb5b09b3de3d4751
StefanW <stefanwetzig@yahoo.de>
2023-11-13 20:23:31 +0100

    add gameparams

---
+++ src/main/java/com/freechazz/network/DTO/GameParams/GameComputeType.java
@@ -0,0 +1,6 @@
+package com.freechazz.network.DTO.GameParams;
+
+public enum GameComputeType
+{
+    MANUEL, HALF_AUTO, AUTO
+}
---
+++ src/main/java/com/freechazz/network/DTO/GameParams/RandomGameParams.java
@@ -0,0 +1,72 @@
+package com.freechazz.network.DTO.GameParams;
+
+import com.freechazz.game.core.ESize;
+
+public class RandomGameParams extends GameParams{
+
+    private ESize size;
+    private boolean withSeed;
+
+    private Long seed;
+    private boolean samePieces;
+    private boolean isBotEnemy;
+    private boolean isAutomatic;
+
+
+    private boolean isNetworkGame;
+    private boolean isPublic;
+
+
+
+
+    public RandomGameParams() {
+    }
+
+
+    public ESize getSize() {
+        return size;
+    }
+    public Long getSeed() {
+        return seed;
+    }
+
+
+            public boolean getIsSamePieces() {
+            return samePieces;
+        }
+
+
+                public boolean getIsPublic() {
+                return isPublic;
+            }
+
+                        public boolean getIsBotEnemy() {
+                        return isBotEnemy;
+                    }
+
+                        public boolean getIsNetworkGame() {
+                        return isNetworkGame;
+                    }
+
+                        public boolean isWithSeed() {
+                        return withSeed;
+                    }
+
+                        public boolean getIsAutomatic() {
+                       return isAutomatic;
+                    }
+
+
+                                public String toString(){
+                        return "RandomGameParams{" +
+                                        "size=" + size +
+                                        ", withSeed=" + withSeed +
+                                        ", seed=" + seed +
+                                       ", samePieces=" + samePieces +
+                                       ", isBotEnemy=" + isBotEnemy +
+                                       ", isNetworkGame=" + isNetworkGame +
+                                       ", isPublic=" + isPublic +
+                                       '}';
+                    }
+        }
+
